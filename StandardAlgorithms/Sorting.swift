977a710e19581f943560b18d7d03f281d82f794b
Koza Kurumlu <Kurumlu.K@etoncollege.org.uk>
2023-11-13 18:29:58 +0000

    Added test and algorithm for merge sort

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -15,4 +15,41 @@ class Sorting {
         }
         return [1,3,4,5,7]
     }
+    func mergeSort(_ array: [Int]) -> [Int] {
+        if array.count <= 1 {
+            return array
+        }
+
+        let middleIndex = array.count / 2
+        let arrayOne = Array(array[..<middleIndex])
+        let arrayTwo = Array(array[middleIndex...])
+
+        func merge(_ left: [Int], _ right: [Int]) -> [Int] {
+            var merged: [Int] = []
+            var leftIndex = 0
+            var rightIndex = 0
+
+            while leftIndex < left.count && rightIndex < right.count {
+                if left[leftIndex] < right[rightIndex] {
+                    merged.append(left[leftIndex])
+                    leftIndex += 1
+                } else if left[leftIndex] > right[rightIndex] {
+                    merged.append(right[rightIndex])
+                    rightIndex += 1
+                } else {
+                    merged.append(left[leftIndex])
+                    leftIndex += 1
+                    merged.append(right[rightIndex])
+                    rightIndex += 1
+                }
+            }
+
+            merged.append(contentsOf: left[leftIndex...])
+            merged.append(contentsOf: right[rightIndex...])
+
+            return merged
+        }
+
+        return merge(mergeSort(arrayOne), mergeSort(arrayTwo))
+    }
 }
--- StandardAlgorithmsTests/SortingTests.swift
+++ StandardAlgorithmsTests/SortingTests.swift
@@ -25,7 +25,22 @@ final class SortingTests: XCTestCase {
             let actual = sorting.bubbleSort(testCase.data)
             XCTAssertEqual(actual, testCase.expected)
         }
+    
         
+    }
+    func testMergeSortWithManySuffledArrayIntegersReturnsThemInAscendingOrder() {
+        //arrange
+        let sorting = Sorting()
+        let testCases = [(data: [5,1,3,4,7], expected: [1,3,4,5,7]),
+                        (data: [8, 2, 6, 9, 0], expected: [0, 2, 6, 8, 9]),
+                        (data: [10, 5, 3, 8, 6], expected: [3, 5, 6, 8, 10]),
+                        (data: [7, 7, 7, 7, 7], expected: [7, 7, 7, 7, 7])]
+        //act
+        //assert
+        for testCase in testCases{
+            let actual = sorting.mergeSort(testCase.data)
+            XCTAssertEqual(actual, testCase.expected)
+        }
     }
 }
  
